4d38b3fd5cb628c4b6a92e1fe3fec92a692c5161
Miriam Kiessecker <miriam@MacBook-Air.local>
2024-12-11 18:24:48 -0600

    nsquare solution found.. optimizing for linear or nlogn solution for day2

--- day2/main.py
+++ day2/main.py
@@ -1,16 +1,69 @@
 with open("day2/input.txt") as file: 
     data = file.read().strip()
+
+
 data = data.split('\n')
 
+
 safe_reports =0
+idx = 0
+size = len (data)
+report = data[idx]
 
-for report in data:
+
+while idx< size: 
+    safe = True
+    ascending = False
+    r_idx = 1
+
+    report = data[idx].split()
+    r_size = len(report)
+
+    delta = int(report[r_idx]) - int(report[r_idx-1])
     
-    report = report.split()
 
-    ascending = (int(report[1])-int(report[0]))
-    print(ascending)
-    for idx in range(len(report)):
-        
-        report[idx]
+
+    idx+=1
+
+
+"""
+while data:    
+    safe = True
+    ascending = False
+    idx = 1
+
+    report = report.split()
+    size = len(report)
+    delta = int(report[idx])-int(report[idx-1])
+    
+    if delta == 0: 
+        continue
+
+    if abs(delta) > 3:
+        continue
+
+
+    if ( delta > 0 ):
+        ascending = True
+    
+    for idx in range(idx+1, size):    
+        delta = int(report[idx])- int(report[idx-1])
+        if ((ascending) and delta < 0): 
+            safe = False
+            break
+
+        if ((not ascending) and delta > 0):
+            safe = False
+            break
+        if abs(delta) > 3:
+            safe = False
+            break
+        if delta == 0:
+            safe = False
+            break
+    
+    if safe: safe_reports+=1
+
+"""
+print (safe_reports)
     #if element:
